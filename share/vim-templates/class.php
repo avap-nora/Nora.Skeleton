ddc7efc53b9fc2fc48f04bfeba86744a9eb0ca56
Hajime MATSUMOTO <hajime.matsumoto@avap.co.jp>
2020-02-03 08:20:56 +0900

    update

--- share/vim-templates/class.php
+++ share/vim-templates/class.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace SomeNamespace;
 
 /**
- * This file is a part of __Vendoor__/__Package__
+ * This file is a part of __Vendor__\__Package__
  *
  * @author __name__
  */
