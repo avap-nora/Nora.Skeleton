f26da2daed0669e3b0b672683673f95fd8e4b0e7
Hajime MATSUMOTO <hajime.matsumoto@avap.co.jp>
2020-02-03 22:15:01 +0900

    update for plugins

---
+++ tests/assets/lib/Kernel/Context/TestConfigurator.php
@@ -0,0 +1,12 @@
+<?php
+namespace __Vendor____Package__\Kernel\Context;
+
+use Nora\Framework\Kernel\AbstractKernelConfigurator;
+
+class TestConfigurator extends AbstractKernelConfigurator
+{
+    public function configure()
+    {
+    }
+}
+
---
+++ tests/assets/lib/Kernel/Kernel.php
@@ -0,0 +1,8 @@
+<?php
+namespace __Vendor____Package__\Kernel;
+
+use Nora\Framework\Kernel\KernelInterface;
+
+class Kernel implements KernelInterface
+{
+}
